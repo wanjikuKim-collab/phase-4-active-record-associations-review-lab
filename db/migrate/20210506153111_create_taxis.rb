683672a75c356bf34400334c1de57699a4f83d84
Wanjiku Kimani <wanjikukimani24@gmail.com>
2023-03-16 08:42:06 +0300

    done

--- db/migrate/20210506153111_create_taxis.rb
+++ db/migrate/20210506153111_create_taxis.rb
@@ -1,6 +1,11 @@
 class CreateTaxis < ActiveRecord::Migration[6.1]
+  # plate example (KBD), plate_number example (176) last_letter (Q)
   def change
     create_table :taxis do |t|
+      t.string :plate
+      t.integer :plate_number
+      t.string :last_letter
+      t.string :driver_name
 
       t.timestamps
     end
